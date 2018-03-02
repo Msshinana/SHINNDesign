f5bee525bed0e1a4fefdf0df0cb63a936a810357
nana <nana@NanadeMacBook-Pro.local>
2018-03-02 16:14:58 +0800

    Initial Commit

---
+++ SHINNDesign/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SHINNDesign
+//
+//  Created by nana on 2018/3/2.
+//  Copyright © 2018年 nana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
